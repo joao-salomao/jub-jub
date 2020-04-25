e54041d0cb0f2143df3f190e3bb6db7075b3bfcb
João Salomão <joaosalomao.613@gmail.com>
2020-04-25 03:14:56 -0300

    Implementando DAO das models AnnotationFile e Annotation

--- lib/dao/annotation_dao.dart
+++ lib/dao/annotation_dao.dart
@@ -1,1 +1,24 @@
-class AnnotationDAO {}
+import 'base_dao.dart';
+import 'package:potato_notes/entities/annotation.dart';
+
+class AnnotationDAO extends BaseDAO<Annotation> {
+  @override
+  String get table => 'annotations';
+
+  @override
+  fromMap(Map<String, dynamic> map) {
+    return Annotation.fromMap(map);
+  }
+
+  Future<List<Annotation>> getAnnotationsByThinkId(int thinkId) async {
+    var dbClient = await db;
+    final list = await dbClient
+        .rawQuery('select * from $table where think_id = ?', [thinkId]);
+
+    if (list.length > 0) {
+      return list.map((map) => fromMap(map)).toList();
+    }
+
+    return [];
+  }
+}
--- lib/dao/annotation_file_dao.dart
+++ lib/dao/annotation_file_dao.dart
@@ -1,1 +1,27 @@
-class AnnotationFileDAO {}
+import 'package:potato_notes/entities/annotation_file.dart';
+
+import 'base_dao.dart';
+
+class AnnotationFileDAO extends BaseDAO<AnnotationFile> {
+  @override
+  String get table => 'annotation_files';
+
+  @override
+  AnnotationFile fromMap(Map<String, dynamic> map) {
+    return AnnotationFile.fromMap(map);
+  }
+
+  Future<List<AnnotationFile>> getAnnotationsFileByAnnotationId(
+    int annotationId,
+  ) async {
+    var dbClient = await db;
+    final list = await dbClient.rawQuery(
+        'select * from $table where annotation_id = ?', [annotationId]);
+
+    if (list.length > 0) {
+      return list.map((map) => fromMap(map)).toList();
+    }
+
+    return [];
+  }
+}
