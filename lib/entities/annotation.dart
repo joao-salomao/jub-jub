f7918d6034d19c6384c8e4d8a2c7695072ce24f6
João Salomão <joaosalomao.613@gmail.com>
2020-04-25 03:15:42 -0300

    Implementando maps e adicionando novos atributos

--- lib/entities/annotation.dart
+++ lib/entities/annotation.dart
@@ -1,13 +1,44 @@
-import 'package:flutter/material.dart';
+import 'entity.dart';
 import 'annotation_file.dart';
+import 'package:flutter/material.dart';
 
-class Annotation {
-  String id;
+class Annotation extends Entity {
+  int id;
+  int thinkId;
   String title;
   String text;
   Color color;
   String createdAt;
   List<AnnotationFile> files = [];
 
-  Annotation(this.id, this.title, this.text, this.createdAt);
+  Annotation({
+    this.id,
+    this.text,
+    this.color,
+    this.files,
+    this.title,
+    this.thinkId,
+    this.createdAt,
+  });
+
+  Annotation.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.thinkId = map['think_id'];
+    this.title = map['title'];
+    this.text = map['text'];
+    this.color = Color(map['color']);
+    this.createdAt = map['createdAt'];
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "think_id": thinkId,
+      "title": title,
+      "text": text,
+      "color": color.value,
+      "createdAt": createdAt
+    };
+  }
 }
--- lib/entities/annotation_file.dart
+++ lib/entities/annotation_file.dart
@@ -1,20 +1,45 @@
 import 'dart:io';
+import 'entity.dart';
 import 'package:video_player/video_player.dart';
 
-class AnnotationFile {
-  String id;
+class AnnotationFile extends Entity {
+  int id;
   File file;
   String path;
   String type;
   String title;
+  int annotationId;
   String description;
   VideoPlayerController controller;
 
-  AnnotationFile(
+  AnnotationFile({
+    this.file,
     this.title,
-    this.file, {
     this.path,
     this.type,
     this.description,
+    this.annotationId,
   });
+
+  AnnotationFile.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.annotationId = map['annotation_id'];
+    this.title = map['title'];
+    this.description = map['description'];
+    this.path = map['path'];
+    this.type = map['type'];
+    this.file = File(map['path']);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "type": type,
+      "path": path,
+      "title": title,
+      "description": description,
+      "annotation_id": annotationId
+    };
+  }
 }
