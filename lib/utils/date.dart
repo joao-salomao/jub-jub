1eff8d40f99d0263b3f08d2de8216d0a16d02e53
João Salomão <joaosalomao.613@gmail.com>
2020-04-26 17:52:16 -0300

    Criando função para formatar Duration

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -2,3 +2,8 @@ import 'package:intl/intl.dart';
 
 String formatDate(date) => new DateFormat('dd/MM/yyyy').format(date);
 
+String formatDuration(Duration value) {
+  final minutes = value.inMinutes.toString().padLeft(2, '0');
+  final seconds = value.inSeconds.remainder(60).toString().padLeft(2, '0');
+  return "$minutes:$seconds";
+}
