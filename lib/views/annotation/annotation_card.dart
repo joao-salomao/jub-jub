c9407eca1dad4321ba2c156051ab335ced82f8f8
João Salomão <joaosalomao.613@gmail.com>
2020-05-01 14:41:34 -0300

    Criando header dinâmico

--- lib/views/annotation/annotation_card.dart
+++ lib/views/annotation/annotation_card.dart
@@ -25,6 +25,122 @@ class AnnotationCard extends StatefulWidget {
 }
 
 class _AnnotationCardState extends State<AnnotationCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      padding: EdgeInsets.all(5),
+      child: InkWell(
+        onTap: _onTapCard,
+        child: Card(
+          color: widget.annotation.color,
+          elevation: 10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: 5,
+                      ),
+                      child: _getHeader(),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        widget.annotation.text,
+                        softWrap: true,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 3),
+                                child: Icon(
+                                  Icons.local_parking,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              AppText(
+                                  "${widget.annotation.getAnnotationTotalWords()}"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 3),
+                                child: Icon(
+                                  Icons.filter,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              AppText(
+                                  "${widget.annotation.getAnnotationFilesCountByType("image")}"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 3),
+                                child: Icon(
+                                  Icons.video_library,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              AppText(
+                                  "${widget.annotation.getAnnotationFilesCountByType("video")}"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 3),
+                                child: Icon(
+                                  Icons.audiotrack,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              AppText(
+                                  "${widget.annotation.getAnnotationFilesCountByType("audio")}"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   _onEnterPassword() {
     return showDialog(
       context: context,
@@ -84,113 +200,24 @@ class _AnnotationCardState extends State<AnnotationCard> {
     _onEnterPassword();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 160,
-      padding: EdgeInsets.all(5),
-      child: InkWell(
-        onTap: _onTapCard,
-        child: Card(
-          color: widget.annotation.color,
-          elevation: 10,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AppText(
-                  widget.annotation.title,
-                  bold: true,
-                  fontSize: 18,
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    widget.annotation.text,
-                    softWrap: true,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.local_parking,
-                              color: Colors.white,
-                            ),
-                          ),
-                          AppText(
-                              "${widget.annotation.getAnnotationTotalWords()}"),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.filter,
-                              color: Colors.white,
-                            ),
-                          ),
-                          AppText(
-                              "${widget.annotation.getAnnotationFilesCountByType("image")}"),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.video_library,
-                              color: Colors.white,
-                            ),
-                          ),
-                          AppText(
-                              "${widget.annotation.getAnnotationFilesCountByType("video")}"),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.audiotrack,
-                              color: Colors.white,
-                            ),
-                          ),
-                          AppText(
-                              "${widget.annotation.getAnnotationFilesCountByType("audio")}"),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+  _getHeader() {
+    final title = AppText(
+      widget.annotation.title,
+      bold: true,
+      fontSize: 18,
+    );
+
+    if (widget.annotation.password == null) return title;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        title,
+        Icon(
+          Icons.lock_outline,
+          color: Colors.white,
+        )
+      ],
     );
   }
 }
