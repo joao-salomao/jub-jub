971fdd7e8344b4ec68b240931eebefac6330bbc6
João Salomão <joaosalomao.613@gmail.com>
2020-07-25 23:10:42 -0300

    Registrando singleton dos daos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:jubjub/dao/think_dao.dart';
 import 'package:jubjub/views-new/app.dart';
+import 'package:jubjub/dao/annotation_dao.dart';
+import 'package:jubjub/dao/annotation_file_dao.dart';
 import 'package:jubjub/controllers/app_controller.dart';
 import 'package:jubjub/controllers/backup_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:jubjub/controllers/app_audio_player_controller.dart';
 
 void setup() async {
+  // DAO's
+  GetIt.I.registerSingleton<ThinkDAO>(ThinkDAO());
+  GetIt.I.registerSingleton<AnnotationDAO>(AnnotationDAO());
+  GetIt.I.registerSingleton<AnnotationFileDAO>(AnnotationFileDAO());
+
   GetIt.I.registerSingleton<AppController>(AppController());
   GetIt.I.registerSingleton<BackupController>(BackupController());
   GetIt.I
