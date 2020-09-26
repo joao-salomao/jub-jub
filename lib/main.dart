08d136ce5e144d7292b39c614c5704f1a6c11d0d
João Salomão <joaosalomao.613@gmail.com>
2020-09-26 00:27:32 -0300

    Registrando todas as services como singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,37 @@
-import 'services/auth_service.dart';
-import 'views/app.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:jubjub/views/app.dart';
 import 'package:jubjub/dao/think_dao.dart';
 import 'package:jubjub/dao/annotation_dao.dart';
+import 'package:jubjub/services/zip_service.dart';
+import 'package:jubjub/services/auth_service.dart';
+import 'package:jubjub/services/backup_service.dart';
 import 'package:jubjub/dao/annotation_file_dao.dart';
 import 'package:jubjub/controllers/app_controller.dart';
-import 'package:jubjub/controllers/backup_controller.dart';
+import 'package:jubjub/services/google_drive_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:jubjub/controllers/app_audio_player_controller.dart';
 
 void setup() async {
-  // Services
-  GetIt.I.registerSingleton<AuthService>(AuthService());
-
   // DAO's
   GetIt.I.registerSingleton<ThinkDAO>(ThinkDAO());
   GetIt.I.registerSingleton<AnnotationDAO>(AnnotationDAO());
   GetIt.I.registerSingleton<AnnotationFileDAO>(AnnotationFileDAO());
 
+  // Controllers
   GetIt.I.registerSingleton<AppController>(AppController());
-  GetIt.I.registerSingleton<BackupController>(BackupController());
   GetIt.I
       .registerSingleton<AppAudioPlayerController>(AppAudioPlayerController());
+
+  // SharedPreferences
   GetIt.I.registerSingletonAsync<SharedPreferences>(
       () => SharedPreferences.getInstance());
+
+  // Services
+  GetIt.I.registerSingleton<AuthService>(AuthService());
+  GetIt.I.registerSingleton<ZipService>(ZipService());
+  GetIt.I.registerSingleton<GoogleDriveService>(GoogleDriveService());
+  GetIt.I.registerSingleton<BackupService>(BackupService());
 }
 
 void main() {
